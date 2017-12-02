63b2b3f8647e7c33e739af808d219ef07e1fcfe1
Simon <zimonitrome@gmail.com>
2017-12-02 18:04:57 +0100

    Upgraded for 2nd voice clip
    Made credit watermark optional
    
    Added moviepy to requirements

--- latd.py
+++ latd.py
@@ -3,15 +3,32 @@ import argparse
 import math
 import moviepy.editor as mp
 
-# CREDIT TO https://twitter.com/Xx__Eric_xX
-LAUGH_AUDIO = mp.AudioFileClip('./laughs/latd_final.m4a')
-TOTAL_SECS = LAUGH_AUDIO.duration
 # 16:9 aspect ratio , default youtube size
 DEFAULT_YT_SIZE = (854, 480)
 # when to cut frames
-START_POINTS = [0, 5, 8, 11.5, 14, 16, 19, 23, 26, 29, 31, 34]
+START_POINTS = []
 
-def main(input_dir, output_file):
+def getAudioPath(audio_version):
+    base_path = './laughs/'
+    file_name = ''
+    if audio_version == '1':
+        file_name = 'v1'
+        START_POINTS.extend([0, 5, 8, 11, 14, 16, 18, 21.5, 24.4])
+        # START_POINTS = [2.5, 4.7, 6.5, 9, 10.5, ]
+    elif audio_version == '2':
+        file_name = 'v2'
+        START_POINTS.extend([0, 5, 8, 11.5, 14, 16, 19, 23, 26, 29, 31, 34])
+    return base_path + file_name + '.m4a'
+
+
+def main(input_dir, output_file, credits_flag, audio_version):
+
+    # select correct audio track
+    # CREDIT TO https://twitter.com/Xx__Eric_xX
+    LAUGH_AUDIO = mp.AudioFileClip(getAudioPath(audio_version))
+    TOTAL_SECS = LAUGH_AUDIO.duration
+
+    # check images and correct amount
     filenames = [x for x in os.listdir(input_dir) if not x.startswith('.')]
     assert len(filenames) >= len(START_POINTS), 'Not enough images to create a good slideshow.'
 
@@ -34,11 +51,12 @@ def main(input_dir, output_file):
         clips.append(clip)
 
     # audio credit to laugh creator
-    watermark = mp.TextClip('audio by @Xx__Eric_xX', color='white', size=(250, None))
-    watermark = watermark.set_opacity(0.7)
-    watermark = watermark.set_pos((570, 440))
-    watermark = watermark.set_duration(TOTAL_SECS)
-    clips.append(watermark)
+    if credits_flag:
+        watermark = mp.TextClip('audio by @Xx__Eric_xX', color='white', size=(250, None))
+        watermark = watermark.set_opacity(0.7)
+        watermark = watermark.set_pos((570, 440))
+        watermark = watermark.set_duration(TOTAL_SECS)
+        clips.append(watermark)
 
     final_clip = mp.CompositeVideoClip(clips, size=DEFAULT_YT_SIZE)
     final_clip = final_clip.set_audio(LAUGH_AUDIO)
@@ -49,9 +67,11 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-i', '--input_dir', help='input directory with source images')
     ap.add_argument('-o', '--output_file', help='output file')
+    ap.add_argument('-c', '--credits', action='store_true', help='enable watermark credit to audio creator')
+    ap.add_argument('-v', '--audio-version', default='2', help='Version of laughter (v.1 for "look at his head", v2 for just laugh)')
     args = ap.parse_args()
 
     assert args.input_dir is not None, 'No input file provided...'
     assert args.output_file is not None, 'No output file provided...'
 
-    main(args.input_dir, args.output_file)
+    main(args.input_dir, args.output_file, args.credits, args.audio_version)
